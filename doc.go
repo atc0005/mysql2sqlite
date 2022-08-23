308392f7cdf2a5921cbc1facf27fedbfc828c48c
Adam Chalkley <atc0005@users.noreply.github.com>
2022-08-23 06:09:44 -0500

    Apply Go 1.19 specific doc comments linting fixes
    These changes are effectively a NOOP for earlier Go versions,
    but improve the formatting of rendered documentation.

--- doc.go
+++ doc.go
@@ -1,28 +1,25 @@
 /*
-
 This repo contains various tools used to mirror MySQL database tables to
 a local SQLite database and validate the results.
 
-
-PROJECT HOME
+# PROJECT HOME
 
 See our GitHub repo (https://github.com/atc0005/mysql2sqlite) for the latest
 code, to file an issue or submit improvements for review and potential
 inclusion into the project.
 
-PURPOSE
+# PURPOSE
 
 Mirror MySQL database tables to SQLite database.
 
-FEATURES
+# FEATURES
 
-• CLI tool for mirroring MySQL database tables to SQLite database
+  - CLI tool for mirroring MySQL database tables to SQLite database
+  - Nagios plugin for validating mirrored SQLite database against the original
+    source
 
-• Nagios plugin for validating mirrored SQLite database against the original source
-
-USAGE
+# USAGE
 
 See our main README for supported settings and examples.
-
 */
 package main
