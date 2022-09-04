25f2c4e593b03b295df15d929bc4985831ff2de3
Adam Chalkley <atc0005@users.noreply.github.com>
2022-09-04 05:42:39 -0500

    Add missing cmd doc files
    Add "package" doc file to resolve revive `package-comment`
    linting error surfaced by recent linter update.

---
+++ cmd/check_mysql2sqlite/doc.go
@@ -0,0 +1,19 @@
+// Copyright 2020 Adam Chalkley
+//
+// https://github.com/atc0005/mysql2sqlite
+//
+// Licensed under the MIT License. See LICENSE file in the project root for
+// full license information.
+
+// Nagios plugin used to validate synchronization status between remote MySQL
+// and local SQLite database.
+//
+// See our [GitHub repo]:
+//
+//   - to review documentation (including examples)
+//   - for the latest code
+//   - to file an issue or submit improvements for review and potential
+//     inclusion into the project
+//
+// [GitHub repo]: https://github.com/atc0005/mysql2sqlite
+package main
---
+++ cmd/mysql2sqlite/doc.go
@@ -0,0 +1,18 @@
+// Copyright 2020 Adam Chalkley
+//
+// https://github.com/atc0005/mysql2sqlite
+//
+// Licensed under the MIT License. See LICENSE file in the project root for
+// full license information.
+
+// CLI app used to mirror a remote MySQL database to a local SQLite database.
+//
+// See our [GitHub repo]:
+//
+//   - to review documentation (including examples)
+//   - for the latest code
+//   - to file an issue or submit improvements for review and potential
+//     inclusion into the project
+//
+// [GitHub repo]: https://github.com/atc0005/mysql2sqlite
+package main
